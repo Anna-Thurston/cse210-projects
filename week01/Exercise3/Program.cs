bd9b5b657e64440849fe027912d2b48591a72634
annathurston <sfcakester@gmail.com>
2025-05-12 23:54:22 -0500

    Finished exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+
+        int guess = 0;
+
+        Console.WriteLine("I'm thinking of a number between 1 and 100.");
+
+
+        while (guess != magicNumber)
+        {
+            Console.Write("Guess the magic number: ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Too low! Try guessing higher.");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Too high! Try guessing lower.");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it! Great job!");
+            }
+        }
     }
 }
