5c13487a1bc727089da6b4dab3b5667088a2352e
annathurston <sfcakester@gmail.com>
2025-05-13 11:46:39 -0500

    Added display of jobs to resume

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+// First job
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+// Second Job
+        Job job2 = new Job();
+        job2._jobTitle = "Senior Developer";
+        job2._company = "Google";
+        job2._startYear = 2022;
+        job2._endYear = 2025;
+
+// Create Resume object
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+
+// Add jobs to resume
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+// Display resume
+        myResume.Display();
     }
 }
