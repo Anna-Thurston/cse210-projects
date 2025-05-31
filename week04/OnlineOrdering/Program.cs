97b41823fe23736fa77aec88a4ae299a0a3692e8
annathurston <sfcakester@gmail.com>
2025-05-31 09:33:51 -0500

    Completed week 4 requirements

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        Address address1 = new Address("123 Elm St", "Provo", "UT", "USA");
+        Customer customer1 = new Customer("Alice Johnson", address1);
+        Order order1 = new Order(customer1);
+        order1.AddProduct(new Product("Notebook", "A001", 2.50, 4));
+        order1.AddProduct(new Product("Pen", "B002", 1.25, 6));
+
+        Address address2 = new Address("45 Queen St", "Toronto", "ON", "Canada");
+        Customer customer2 = new Customer("Bob Smith", address2);
+        Order order2 = new Order(customer2);
+        order2.AddProduct(new Product("Book", "C003", 15.00, 1));
+        order2.AddProduct(new Product("Backpack", "D004", 30.00, 1));
+        order2.AddProduct(new Product("Water Bottle", "E005", 10.00, 2));
+
+        DisplayOrder(order1);
+        Console.WriteLine("----------------------------");
+        DisplayOrder(order2);
+    }
+
+    static void DisplayOrder(Order order)
+    {
+        Console.WriteLine("Packing Label:");
+        Console.WriteLine(order.GetPackingLabel());
+        Console.WriteLine("Shipping Label:");
+        Console.WriteLine(order.GetShippingLabel());
+        Console.WriteLine("Total Price: $" + order.GetTotalPrice().ToString("0.00"));
+        Console.WriteLine();
     }
 }
--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        List<Video> videos = new List<Video>();
+
+        Video video1 = new Video("How to Tie Shoelaces", "Samuel Costa", 100);
+        video1.AddComment(new Comment("Alice", "Great tutorial!"));
+        video1.AddComment(new Comment("Bob", "I was waiting for this watch."));
+        video1.AddComment(new Comment("Carol", "Variation details?"));
+
+        Video video2 = new Video("How to Cook Spaghetti", "Brendan Brady", 300);
+        video2.AddComment(new Comment("Dave", "Awesome selection!"));
+        video2.AddComment(new Comment("Emma", "Where is the sauce?"));
+        video2.AddComment(new Comment("Frank", "Loved the homemade pasta."));
+
+        Video video3 = new Video("How to breakdance", "Anna Thurston", 500);
+        video3.AddComment(new Comment("Grace", "So helpful!"));
+        video3.AddComment(new Comment("Hank", "Clear and concise."));
+        video3.AddComment(new Comment("Ivy", "Subscribed!"));
+
+        Video video4 = new Video("How to Cook Chicken", "Ugochukwe Febechukwu", 450);
+        video4.AddComment(new Comment("Jake", "Tried this yesterday, amazing."));
+        video4.AddComment(new Comment("Laura", "Can you do one on lasagna?"));
+        video4.AddComment(new Comment("Mike", "Thanks for the tips!"));
+
+        videos.Add(video1);
+        videos.Add(video2);
+        videos.Add(video3);
+        videos.Add(video4);
+
+        foreach (Video video in videos)
+        {
+            Console.WriteLine("Title: " + video.GetTitle());
+            Console.WriteLine("Author: " + video.GetAuthor());
+            Console.WriteLine("Length: " + video.GetLengthInSeconds() + " seconds");
+            Console.WriteLine("Number of Comments: " + video.GetCommentCount());
+            Console.WriteLine("Comments:");
+
+            List<Comment> comments = video.GetComments();
+            foreach (Comment comment in comments)
+            {
+                Console.WriteLine("- " + comment.GetCommenterName() + ": " + comment.GetText());
+            }
+
+            Console.WriteLine(new string('-', 40));
+        }
     }
 }
