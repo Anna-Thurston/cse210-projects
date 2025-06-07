1e8abce666bab71d1a18fa4af1e5164c5a3a2cd9
annathurston <sfcakester@gmail.com>
2025-06-07 11:29:17 -0500

    Completed Inheritance Learning Activity

---
+++ week05/Homework/MathAssignment.cs
@@ -0,0 +1,20 @@
+public class MathAssignment : Assignment
+{
+    // Private member variables
+    private string _textbookSection;
+    private string _problems;
+
+    // Constructor
+    public MathAssignment(string studentName, string topic, string textbookSection, string problems)
+        : base(studentName, topic) // Call the base class constructor
+    {
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+
+    // Method to get the homework list
+    public string GetHomeworkList()
+    {
+        return $"Section {_textbookSection} Problems {_problems}";
+    }
+}
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Create an instance of the Assignment class
+        Assignment myAssignment = new Assignment("Mary Waters", "Fractions");
+        string summary = myAssignment.GetSummary();
+        Console.WriteLine(summary);
+
+
+        // Test MathAssignment
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        Console.WriteLine();
+
+
+        // Test WritingAssignment
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
